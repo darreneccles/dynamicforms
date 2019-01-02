7a99a4fae61c9275a87ac4cdbac7b53c21ac4a06
Darren Eccles <darren@emerald-design.co.uk>
2019-01-02 14:04:32 +0000

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, copyArrayItem, transferArrayItem} from '@angular/cdk/drag-drop';
+
+import { DynamicFormModel, DynamicFormService,DynamicInputModel, DynamicTextAreaModel, DynamicFormControlComponent,DynamicFormControlModel, DynamicCheckboxModel } from "@ng-dynamic-forms/core";
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,112 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  previewFormModel: DynamicFormModel = [];
+  previewFormGroup: FormGroup;
+  formModel: DynamicFormModel = [];
+  formGroup: FormGroup;
+  fieldData: DynamicFormControlModel;
+  fieldDataJSON = '';
+  formData = null;
+  fieldDataType ='';
+
+  trackByFn(index: any, item: any) {
+    return index;
+  }
+
+  editField(id) {
+    var formField = this.formModel.find(x => x.id == id);
+    
+    if(formField != null)
+    {
+      this.fieldData = formField;
+      this.fieldDataType = formField.type;
+      //this.fieldDataJSON = JSON.stringify(this.fieldData);
+    }
+  }
+
+  updateField(fieldattribute,field)
+  {
+    var formField;
+    switch(field.type) {
+      case "INPUT": {
+        formField = this.formModel.find(x => x.id == field.id) as DynamicInputModel;
+        break;
+      }
+      case "TEXTAREA": {
+        formField = this.formModel.find(x => x.id == field.id) as DynamicTextAreaModel;
+        break;
+      }
+      case "CHECKBOX": {
+        formField = this.formModel.find(x => x.id == field.id) as DynamicCheckboxModel;
+        break;
+      }
+    }
+    if (fieldattribute.key == "_value") {
+      formField.valueUpdates.next(fieldattribute.value);
+    } else {
+      this.fieldData[fieldattribute.key] = fieldattribute.value;
+    }
+    // alternatively, if your not bothered about updating the value, then comment the above out,
+    // and uncomment below:
+    //this.fieldData[fieldattribute.key] = fieldattribute.value;
+  }
+
+  addTextBox() {
+    var dynamicInputModel = new DynamicInputModel({
+      id:(this.formModel.length+1).toString(),
+      label:"Textbox",
+      maxLength:20,
+      placeholder:"Placeholder Text",
+      value:""
+    });
+    this.formModel.push(dynamicInputModel);
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+    this.formData = this.formModel;
+  }
+
+  addTextArea() {
+    var dynamicTextAreaModel = new DynamicTextAreaModel({
+      id:(this.formModel.length+1).toString(),
+      label:"Textarea",
+      placeholder: "Placeholder Text",
+      value:""
+    })
+    this.formModel.push(dynamicTextAreaModel);
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+    this.formData = this.formModel;
+  }
+
+  addCheckbox() {
+    var dynamicCheckboxModel = new DynamicCheckboxModel({
+      id:(this.formModel.length+1).toString(),
+      label:"Checkbox"
+    })
+    this.formModel.push(dynamicCheckboxModel);
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+    this.formData = this.formModel;
+  }
+
+  previewForm() {
+    this.previewFormModel = this.formModel;
+    this.previewFormGroup = this.formService.createFormGroup(this.previewFormModel);
+  }
+
+  constructor(private formService: DynamicFormService) {}
+
+    ngOnInit() {
+        this.formGroup = this.formService.createFormGroup(this.formModel);
+        this.previewFormGroup = this.formService.createFormGroup(this.formModel);
+    }
+
+    drop(event: CdkDragDrop<string[]>) {
+      if (event.previousContainer === event.container) {
+        // sort
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      } else {
+        // drag and drop
+        copyArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);                
+      }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DynamicFormsCoreModule } from "@ng-dynamic-forms/core";
+import { DynamicFormsBootstrapUIModule } from "@ng-dynamic-forms/ui-bootstrap";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormarrayComponent } from './formarray/formarray.component';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormarrayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    ReactiveFormsModule,
+    DynamicFormsCoreModule,
+    DynamicFormsBootstrapUIModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
